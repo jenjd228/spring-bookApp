a39c4540cf9c0e9145764a1fd738d7164022586c
jenjd228 <ekocaba2@mail.ru>
2021-02-05 19:38:40 +0300

    locale

--- src/main/java/com/example/BookApp/config/LocaleChangeConfig.java
+++ src/main/java/com/example/BookApp/config/LocaleChangeConfig.java
@@ -1,9 +1,7 @@
 package com.example.BookApp.config;
 
-import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -16,29 +14,29 @@ import java.util.Locale;
 @Configuration
 public class LocaleChangeConfig implements WebMvcConfigurer {
 
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+
     @Bean
-    public LocaleResolver localeResolver(){
+    public LocaleResolver localeResolver() {
         SessionLocaleResolver localeResolver = new SessionLocaleResolver();
         localeResolver.setDefaultLocale(Locale.ENGLISH);
         return localeResolver;
     }
 
     @Bean
-    public LocaleChangeInterceptor localeChangeInterceptor(){
+    public LocaleChangeInterceptor localeChangeInterceptor() {
         LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
         localeChangeInterceptor.setParamName("lang");
         return localeChangeInterceptor;
     }
 
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(localeChangeInterceptor());
-    }
-
-    @Bean(name = "messageResourceSB")
-    public MessageSource messageResource() {
-        ResourceBundleMessageSource messageBundleResrc=new ResourceBundleMessageSource();
-        messageBundleResrc.setBasename("classpath:messages");
-        return messageBundleResrc;
+    @Bean()
+    public ResourceBundleMessageSource messageResource() {
+        ResourceBundleMessageSource resourceBundleMessageSource = new ResourceBundleMessageSource();
+        resourceBundleMessageSource.setBasename("classpath:lang/messages");
+        return resourceBundleMessageSource;
     }
 }
